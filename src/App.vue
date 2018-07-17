77e83d0328de82fe201f8f7da84467754417eafa
Paula Miceli <mariapaulamiceli@gmail.com>
2018-07-17 18:25:29 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+       <arreglo :multiple="true" v-on:changed="viewnewdata" v-bind:TiposCobertura="TiposCobertura"></arreglo>
+    </div>
+ </div>
+</template>
+
+<script>
+import arreglo from './ArregloIntermedio.vue'
+
+  export default {
+    components : {
+      arreglo
+    },
+    data () {
+      return {
+        TiposCobertura: [
+          {
+            texto: 'Tipo de cobertura A'
+          },
+          {
+            texto: 'Tipo de cobertura B'
+          },
+          {
+            texto: 'Tipo de cobertura C'
+          },
+          {
+            texto: 'Tipo de cobertura D'
+          },
+          {
+            texto: 'Tipo de cobertura E'
+          }
+        ],
+      }
+    },
+    methods: {
+      viewnewdata (value) {
+        console.log(value);
+        
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/ArregloIntermedio.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <form v-if=" !multiple" >
+       <ul v-for="tipos in TiposCompletos">
+          <input type="checkbox" id="info" v-bind:value="tipos.data" v-model="tipos.checked">
+              <label for="tipos.data"> {{tipos.data}} </label>
+              <div v-if='tipos.checked'>
+                <input type="text" class="form-control" placeholder="" value="Cubre hasta : $" v-model='tipos.dato'>
+              </div> 
+       </ul>
+    </form>
+
+
+    <form v-else >
+       <ul v-for="tipos in TiposCompletos">
+          <input type="radio" id="info" name="datos" v-bind:value="tipos.data" v-model="tiporadio">
+              <label for="tipos.data"> {{tipos.data}} </label>
+              <div v-if='tiporadio === tipos.data'>
+                <input type="text" class="form-control" placeholder="" value="Cubre hasta : $" v-model='tipos.dato'>
+              </div> 
+       </ul>
+    </form>
+
+
+  </div>
+</template>
+
+
+<script>
+  export default {
+    props : [
+      'TiposCobertura',
+      'multiple'
+    ],
+    data () {
+      return {
+        TiposCompletos: [
+        
+        ],
+        tiporadio: null,
+      }
+    }, 
+    methods : {
+      agregar(){
+        this.TiposCobertura.forEach(element => {
+              this.TiposCompletos.push({
+              data : element.texto,
+              dato : '',
+              checked : false,
+            })
+        });
+      },
+      agregarFinal(){
+        var TiposFinales = [];
+        this.TiposCompletos.forEach(element => {
+          if(element.checked == true && element.dato>0 && element.dato!=null){
+              TiposFinales.push({
+              data : element.data,
+              dato : element.dato,
+            })
+          }
+        });
+        return TiposFinales;
+      }
+    },
+    mounted(){
+      this.agregar();
+    },
+    watch : {
+      TiposCompletos: {
+        handler: function(newValue){
+          this.$emit('changed', this.agregarFinal());
+        },
+        deep:true
+      }
+    }
+      
+}
+</script>
